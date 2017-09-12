031891d5fd7e3baf2341b7d1fadcf4bbfa9d7b8a
pulkjr <pulkjr@gmail.com>
2017-09-12 13:14:58 -0400

    Updated the Session.ps1 for Jason.a.k.a Slave driver

--- Sessions.ps1
+++ Sessions.ps1
@@ -1,27 +1,75 @@
-# Create a Person Object
-$person = New-Object -TypeName pscustomobject -Property @{
+# A Person enters the session lets record their properties
+$Jason = New-Object -TypeName pscustomobject -Property @{
     Name = "Jason";
     Height = 100;
     Haircolor = "grey";
-    CurrentSession = "General"
+    CurrentSession = "PowerShell";
+    Presenter = $true;
+    Company = "NetApp";
+    JobTitle = "Professional Services Consultant"
 }
+$Jason
+#Another Person enters session
 
-#Import Insight atendees and session schedules
-$SessionsSchedule = Import-Csv -Path 
+$Joseph = New-Object -TypeName pscustomobject -Property @{
+    Name = "Joseph"
+    Height = 69
+    Haircolor = "Brown"
+    CurrentSession = "PowerShell"
+    Presenter = $true
+    Company = "NetApp"
+    JobTitle = "Professional Services Engineer"
+}
+$Joseph
 
-#Display attendees signed up for sessions
-$SessionsSchedule
+#I need a place to hold all of these people. 
+$sessionArr = @()
+$sessionArr += $Jason
+$sessionArr += $Joseph
+$sessionArr
 
-#Get a list of distinct attendees
-$SessionsSchedule | Select-Object -Unique Name
+#I now have an array of objects. I can validate this by running the following method
+$sessionArr.gettype()
 
-#Place Attendees in a variable
-$InsightAttendeesArr = $SessionsSchedule | Select-Object -Unique Name
+#I can filter the array using the where-object statement
+$sessionArr | Where-Object{$_.Name -eq "Jason"}
 
-#Find All attendees slotted for this session
-$InsightAttendeesArr | Where-Object {$_.Session -eq "746"}
+#I can put jason back into a variable
+$JasonObj = $sessionArr | Where-Object{$_.Name -eq "Jason"}
+$JasonObj
 
-#Mark all attendees as currently present in session
-foreach($attendee in $InsightAttendeesArr | Where-Object {$_.Session -eq "746"}){
-        $attendee.Status = "In Session"
+#I can see that jasons name is also an object
+$JasonObj.Name.gettype()
+
+#I can update a property of Jason and it will reflect back to the object in the array, as they are one in the same.
+$JasonObj.Company = "The Best Company... NETAPP"
+$JasonObj
+$sessionArr
+$JasonObj.Company = "NetApp"
+
+# That was fun but I need a way to make these people faster as there are a lot of us. Lets create a function
+function New-Person{
+    param(
+        $Name,
+        $Height,
+        $HairColor,
+        $CurrentSession,
+        $Presenter,
+        $Company,
+        $JobTitle,
+        $MaxBeers = 10
+    )
+    $returnObj = New-Object -TypeName pscustomobject -Property @{
+        Name = $Name
+        Height = $Height
+        Haircolor = $HairColor
+        CurrentSession = $CurrentSession
+        Presenter = $Presenter
+        Company = $Company
+        JobTitle = $JobTitle
+        MaxBeers = $MaxBeers
+    }
+    $returnObj.TypeNames.Add('NetApp.Insight.Attendee')
+
+    $returnObj
 }
